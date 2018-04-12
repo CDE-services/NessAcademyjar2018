5ea76183fc890769e28f099b0ce3b18bccd50095
Michaela Bačíková <michaela.bacikova@tuke.sk>
2018-04-12 14:56:35 +0200

    pridany priklad so shapes

---
+++ HelloWorld/src/shapes/Square.java
@@ -0,0 +1,35 @@
+package shapes;
+
+public class Square extends Shape {
+	private int size;
+	
+	public Square(int size) {
+		super();
+		this.size = size;
+	}
+	
+	public Square(int size, int x, int y, char color) {
+		super(x, y, color);
+		this.size = size;
+	}
+	
+	public int getSize() {
+		return size;
+	}
+	
+	public void setSize(int size) {
+		this.size = size;
+	}
+
+	@Override
+	public void print() {
+		super.printOffsetY();
+		for(int r = 0; r < size; r++) {
+            for(int c = 0; c < size; c++) {
+            	super.printOffsetX();
+                System.out.print(super.color);
+            }
+            System.out.println();
+        }
+	}
+}
